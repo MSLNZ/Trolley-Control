9856801954637a4f5b0840330b238a86e14df724
Chris Young <c.young@callaghaninnovation.govt.nz>
2019-10-02 16:28:16 +1300

    Create PTU303.cs
    Class to query PTU303 not yet implemented

---
+++ PTU303.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trolley_Control
+{
+    class PTU303:Barometer
+    {
+
+        public PTU303(ref VaisalaUpdateGui pbarug)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (NotImplementedException)
+            {
+                MessageBox.Show("PTU303 has not been implemented yet");
+
+            }
+        }
+
+        public override void Close()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (NotImplementedException)
+            {
+                MessageBox.Show("PTU303 has not been implemented yet");
+
+            }
+         
+        }
+
+        public override bool IsOpen()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (NotImplementedException)
+            {
+                MessageBox.Show("PTU303 has not been implemented yet");
+                return false;
+            }
+            
+        }
+
+        public override double getPressure()
+        {
+            return result + current_correction;
+        }
+
+
+    }
+}
